92329075f4b9544f695a56fa5b1f0c92cec814d2
Sprootch <31722510+Sprootch@users.noreply.github.com>
2017-09-12 08:04:49 +0200

    Extract a Proxy class

---
+++ Proxy.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace Core
+{
+    public class Proxy
+    {
+        public IWebProxy WebProxy { get; private set; }
+
+        public Proxy(string address, int port, string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(address) ||
+                string.IsNullOrWhiteSpace(username) ||
+                string.IsNullOrWhiteSpace(password) ||
+                port == default(int))
+            {
+                WebProxy = WebRequest.GetSystemWebProxy();
+            }
+            else
+            {
+                WebProxy = new WebProxy(address, port)
+                {
+                    BypassProxyOnLocal = false,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(username, password)
+                };
+            }
+        }
+    }
+}
